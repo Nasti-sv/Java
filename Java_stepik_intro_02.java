d58b7baf11533f0ba9d71c84be1bcd7a0691fb84
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-05-20 18:18:42 +0300

    Update Java_stepik_intro_02.java

--- Java_stepik_intro_02.java
+++ Java_stepik_intro_02.java
@@ -360,24 +360,18 @@ class MyProgram {
 }
 
 /*На вход подаётся строка текста. Выведите её первый и последний элемент в одну строку через пробел.
-1.импортируем сканер.
-2.создаем ввод строки.
-3.получаем элемент строки по его индексу charAt();
-4.узнаем длину строки(т.к. она может быть длиннее чем в примере) через str.length();
-5.получаем элемент строки по индексу ссылаясь на length();
-6.выводим.
 Оставлю ссылку в которой есть прекрасный пример того, как определить  последний символ в строке 
 http://study-java.ru/uroki-java/rabota-so-strokami-v-java-osnovnye-metody-klassa-string/ */
 
-import java.util.Scanner;
+import java.util.Scanner; //1.импортируем сканер.
 class MyProgram {
    public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
-   String imput = sc.nextLine();
-   char letter_0 = imput.charAt(0);
-   int last = imput.length()-1;
-   char letter_last = imput.charAt(last);
-   System.out.println(letter_0+" "+letter_last);
+   String imput = sc.nextLine(); //2.создаем ввод строки.
+   char letter_0 = imput.charAt(0); //3.получаем элемент строки по его индексу charAt();
+   int last = imput.length()-1; //4.узнаем длину строки(т.к. она может быть длиннее чем в примере) через str.length();
+   char letter_last = imput.charAt(last); //5.получаем элемент строки по индексу ссылаясь на length();
+   System.out.println(letter_0+" "+letter_last); //6.выводим.
    }
 }
 
