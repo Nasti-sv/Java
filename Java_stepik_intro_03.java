81ec55e0a3702a93303eb13e0a7089de9baedb52
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-05-22 20:53:42 +0300

    Update Java_stepik_intro_03.java

--- Java_stepik_intro_03.java
+++ Java_stepik_intro_03.java
@@ -58,3 +58,51 @@ class MyProg {
                }
 	}
 }
+//Выведите на печать наибольшее из трёх целых чисел.
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a>b){
+            System.out.println(a);
+        }else if (b>c) {
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
+	}
+}
+/* В 1997 году во время боксёрского боя за звание чемпиона мира Майк Тайсон откусил ухо Эвандеру Холифилду (не целиком). 
+Рефери нужно определить, как поступить. Если откушенный кусок уха меньше норматива, бой продолжится.
+Если нет - Майк Тайсон должен быть дисквалифицирован, и чемпионом становится Холифилд.
+На вход подаются два числа -  масса откушенного куска уха и норматив. Выведите "Бой продолжается!" или "Холифилд - чемпион!" в зависимости от выполнения условия.*/
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+            double piece = sc.nextDouble();
+            double rule = sc.nextDouble();
+        if (piece<rule) {
+            System.out.println("Бой продолжается!");
+        }else{
+            System.out.println("Холифилд - чемпион!");
+        }
+        
+	}
+}
+//На вход подаётся целое число n. Выведите "YES", если можно построить правильный многогранник из правильных плоских n-угольников, в противном случае - "NO".
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number>=3 && number<=5){
+        System.out.println("YES");
+        }else{
+        System.out.println("NO");
+        }
+	}
+}
