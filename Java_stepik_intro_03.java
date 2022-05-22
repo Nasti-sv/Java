5dd36df1dcdeb23f4e850b71ab6d8cf2c7797c44
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-05-22 20:12:40 +0300

    Create Java_stepik_intro_03.java

---
+++ Java_stepik_intro_03.java
@@ -0,0 +1,60 @@
+//Исправьте ошибки в коде таким образом, чтобы код выводил на печать слово "YES" (без кавычек), если значение переменной х равно 5.
+class Example {
+	public static void main(String[] args) {
+		int x = 5;
+        if (x == 5) {
+            System.out.println("YES");
+        }
+
+        }	
+}
+/*Два индийских программиста, Махатма и Джавахарлал, решили выяснить, кто из них быстрее пишет код. Напишите программу, которая поможет им выяснить это.
+На вход подаются два целых числа в одной строке через пробел. Если первое число больше второго, необходимо вывести на печать "Махатма", если второе - 
+"Джавахарлал". Гарантируется, что числа больше нуля и различны.*/
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int firstNumber = sc.nextInt();
+        int secondNumber = sc.nextInt();
+        if (firstNumber > secondNumber) {
+            System.out.println("Махатма");
+            }else{
+            System.out.println("Джавахарлал");
+        }
+	}
+}
+/*Два индийских программиста, Махатма и Джавахарлал, решили выяснить, чей код длиннее. Напишите программу, которая поможет им выяснить это. 
+На вход подаются две строки текста. Если первая строка длиннее второй, необходимо вывести на печать "Махатма", если наоборот - "Джавахарлал".
+Гарантируется, что строки имеют разную длину.*/
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+            String a = sc.nextLine();
+            int x = a.length();
+            String b = sc.nextLine();
+            int h = b.length();
+        if  (x > h) {
+            System.out.println("Махатма");
+        }else{
+            System.out.println("Джавахарлал");
+        }
+	}
+}
+
+/*На первой строке подаётся пароль, хранящийся в базе данных, на второй - пароль, введённый пользователем.
+Выведите на печать фразу "Access is granted", если пользователь ввёл верный пароль, и "Access is denied" - если нет. */
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String databasePassword = sc.next();
+        String password = sc.next();
+               if (databasePassword.equals(password)) {
+                   System.out.println("Access is granted");
+               }else{
+                   System.out.println("Access is denied");
+               }
+	}
+}
