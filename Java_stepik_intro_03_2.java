a09cf559eb211b48099d192540e2e5eb0a38c456
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-05-23 20:06:09 +0300

    Create Java_stepik_intro_03_2.java

---
+++ Java_stepik_intro_03_2.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String[] args) {
+		int age = 23, tasks = 4000;
+        if (age > 21 & tasks > 500) {
+            System.out.print("Добро пожаловать!");
+        }
+	}
+}
+
+//
