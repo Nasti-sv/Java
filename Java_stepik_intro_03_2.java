39dffb074c33b73d082999b28b7e1801e94f27d3
Anastasiya Savchuk <nastyasavchuk9709@gmail.com>
2022-05-23 20:27:25 +0300

    Update Java_stepik_intro_03_2.java

--- Java_stepik_intro_03_2.java
+++ Java_stepik_intro_03_2.java
@@ -8,3 +8,18 @@ class Main {
 }
 
 //На вход подаётся строка, а затем - одна буква. Если строка содержит эту букву или строка длиннее 20 символов, выведите "YES", в противном случае - "NO".
+import java.util.Scanner;
+class mySolution{
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String imputLine = sc.nextLine();
+       String letter = sc.nextLine();
+        if (imputLine.contains(letter) || imputLine.length()>20) {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+   }
+}
+       
+ //
